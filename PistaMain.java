f90124d6dfe10ff3955696cba508af1d612d80a4
LuizAntVR <luiz.antonioviela@gmail.com>
2025-07-03 21:34:28 -0300

    Corrida

---
+++ PistaMain.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Pista;
+
+/**
+ *
+ * @author aluno
+ */
+public class PistaMain {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
